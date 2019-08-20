0a6141636bebd197304846fe96dcc118912a12ca
Blake Goms <blakegoms@gmail.com>
2019-08-20 14:09:58 -0700

    mvp

--- api/server.js
+++ api/server.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
+const knexConnection = require('../data/db-config')
 const projectRouter = require('../project/users-router.js')
 
 const server = express();
 
+// session configuration
+const sessionOptions = {
+    name: 'Session',
+    secret: process.env.COOKIE_SECRET || 'keep it secret, keep it safe!', // for encryption
+    cookie: {
+        secure: process.env.COOKIE_SECURE || false, // in production should be true, false for development
+        maxAge: 1000 * 60 * 60 * 24, // how long is the session good for, in milliseconds
+        httpOnly: true, // client JS has no access to the cookie
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new KnexSessionStore({
+        knex: knexConnection,
+        createtable: true,
+        clearInterval: 1000 * 60 * 60, // how long before we clear out expired sessions
+    }),
+};
+server.use(helmet());
 server.use(express.json());
+server.use(cors());
+server.use(session(sessionOptions));
+
+
 server.use('/api', projectRouter);
+
+server.get('/', (req, res) => {
+    res.json({ api: 'up and running', session: req.session });
+});
 
 module.exports = server;
--- project/users-router.js
+++ project/users-router.js
@@ -1,19 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const server = require('express').Router();
 const bcrypt = require('bcryptjs');
 
-const db = require('../data/db-config');
 const Users = require('./users-model');
 const restricted = require('../auth/restricted-middleware');
-const server = express();
 
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
-
-
-server.post('/register', (req,res) =>{
+server.post('/auth/register', (req,res) =>{
     const user = req.body;
     const hash = bcrypt.hashSync(user.password);
     user.password = hash;
@@ -26,23 +17,36 @@ server.post('/register', (req,res) =>{
         })
 })
 
-server.post('/login', (req,res) =>{
+server.post('/auth/login', (req,res) =>{
     const {username, password} = req.body;
 
     Users.findBy({ username })
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
-                res.status(200).json({ message: `We meet again, ${user.username}!` });
-            } else {
-                res.status(401).json({ message: 'YOU SHALL NOT PASS!!!' });
-            }
+                req.session.username = user.username; // only on successful login
+                req.session.loggedIn = true;
+                res.status(200).json({
+                    message: `Welcome ${user.username}!`,
+                });
+                } else {
+                res.status(401).json({ message: 'Invalid Credentials' });
+                }
         })
         .catch(error => {
             res.status(500).json(error);
         });
 })
 
+server.get('/auth/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.status(200).json({ bye: 'log back in soon!' });
+    });
+});
+
+
+
+// gets all users
 server.get('/users', restricted, (req, res) => {
     Users.find()
         .then(users => {
